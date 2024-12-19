0075e9e3fb4e1456546133d31875ba0c924a21a7
LamVyHuynh <huynhlamvy145@gmail.com>
2024-12-19 18:18:17 +0700

    bai variable

---
+++ Variable/index.js
@@ -0,0 +1,19 @@
+// // prompt("Nhập tên của bạn vào:");
+
+// // Khai báo biến là chỉ thuần là khai báo biến
+// var myNam;
+
+// // Khởi tạo biến là khai báo biến + gán giá trị cho biến
+// var myNam = "Nguyen Van A";
+// console.log(myNam);
+
+// let yourName = "Nguyen Van B";
+// console.log(yourName);
+
+// const age = 20;
+// console.log(age);
+
+prompt("Nhập tên của bạn vào: ");
+var MyName = "Trần Như Nhộng";
+console.log(MyName);
+console.log("Chao mung " + MyName);
