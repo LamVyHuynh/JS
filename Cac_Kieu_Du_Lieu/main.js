c49611ddfe14d966b3f141b26de4801c6edd5837
LamVyHuynh <huynhlamvy145@gmail.com>
2024-12-20 11:51:09 +0700

    cac kieu du lieu

---
+++ Cac_Kieu_Du_Lieu/main.js
@@ -0,0 +1,28 @@
+let myString = "Xin chào";
+console.log(myString);
+console.log(typeof myString);
+
+let mySoNguyen = 10;
+console.log(mySoNguyen);
+console.log(typeof mySoNguyen);
+
+let mySoThuc = 10.5;
+console.log(mySoThuc);
+console.log(typeof mySoThuc);
+
+console.log("Số Nguyên an toàn tối đa: " + Number.MAX_SAFE_INTEGER);
+console.log("Số Nguyên an toàn tối thiểu: " + Number.MIN_SAFE_INTEGER);
+
+let myBooLean = true;
+console.log(myBooLean);
+console.log(typeof myBooLean);
+
+// undefined
+let myUndefined;
+console.log(myUndefined);
+console.log(typeof myUndefined);
+
+// null
+let myNull = null;
+console.log(myNull);
+console.log(typeof myNull);
