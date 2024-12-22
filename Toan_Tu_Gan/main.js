a72e73fb58fe0dec3b9c113462d6aea3cd812d18
LamVyHuynh <huynhlamvy145@gmail.com>
2024-12-22 10:22:39 +0700

    Toan Tu Gan

---
+++ Toan_Tu_Gan/main.js
@@ -0,0 +1,37 @@
+// Toán tử gán =
+// let a = 10;
+// let b = 5;
+// let s = a + b;
+// console.log(a);
+// console.log(b);
+// console.log(s);
+
+// // Toán tử gán += , -=, *=, /=, %=
+// console.log(`Kết quả a+=b = ${(a += b)}`);
+// console.log(`Kết quả a-=3 =${(a -= 3)}`);
+// console.log(`Kết quả b*=3 =${(b *= 3)}`);
+// console.log(`Kết quả a/=3 =${(a /= 3)}`);
+// console.log("Kết quả lấy dư b%=4=%s", (b %= 4));
+
+//BT
+let a = 7;
+console.log("a=", a);
+a += 3;
+console.log("a =", a);
+
+let b = 15;
+b -= 6;
+console.log("b =", b);
+
+let c = 5;
+c *= 4;
+console.log("c =", c);
+
+let d = 12;
+d /= 3;
+console.log("d =", d);
+
+let e = 5,
+  f = 2;
+e = e - (f + 1);
+console.log("e =", e);
