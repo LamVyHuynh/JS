8fd7d047c2dcc4d2e311630993c63d1d5209a345
LamVyHuynh <huynhlamvy145@gmail.com>
2025-01-07 15:53:17 +0700

    lam xong bt10 va sua lai ep thanh kieu int cho gia tri nhap vao

--- If_Else_Else-if/bt09/bt09.js
+++ If_Else_Else-if/bt09/bt09.js
@@ -1,4 +1,4 @@
-let nam = prompt("Nhập số năm vào: ");
+let nam = parseInt(prompt("Nhập số năm vào: "));
 if ((nam % 4 === 0 && nam % 100 !== 0) || nam % 400 === 0) {
   console.log("Năm nhuận");
 } else {
---
+++ If_Else_Else-if/bt10/bt10.js
@@ -0,0 +1,22 @@
+let thang = parseInt(prompt("Nhập số tháng vào: "));
+let nam;
+if (
+  thang === 1 ||
+  thang === 3 ||
+  thang === 5 ||
+  thang === 7 ||
+  thang === 8 ||
+  thang === 10 ||
+  thang === 12
+) {
+  console.log("Tháng có 31 ngày");
+} else if (thang === 4 || thang === 6 || thang === 9 || thang === 11) {
+  console.log("Tháng có 30 ngày");
+} else {
+  nam = parseInt(prompt("Nhập số năm vào: "));
+  if ((nam % 4 === 0 && nam % 100 !== 0) || nam % 400 === 0) {
+    console.log("Tháng này có 29 ngày");
+  } else {
+    console.log("Tháng này có 28 ngày");
+  }
+}
