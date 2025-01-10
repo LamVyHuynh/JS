e4e01249fe6457336d56dd819eff2396e24e8627
LamVyHuynh <huynhlamvy145@gmail.com>
2025-01-10 16:10:33 +0700

    do-while va while tim so chan

---
+++ Whlie/timSoChan/main.js
@@ -0,0 +1,8 @@
+let i = 1;
+let n = Number(prompt("Nhập số n vào: "));
+while (i <= n) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+  i++;
+}
---
+++ do_while/kiemTraChanLe/main.js
@@ -0,0 +1,8 @@
+let i = 1;
+let n = Number(prompt("Nhập số n vào: "));
+do {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+  i++;
+} while (i <= n);
