d57db06fe4b54f541380f5017facb5d4f22a9ee8
LamVyHuynh <huynhlamvy145@gmail.com>
2025-01-12 22:00:15 +0700

    sua lai chut xiu

--- bt_Vong_Lap_For_While/For/timSoHoanHao/main.js
+++ bt_Vong_Lap_For_While/For/timSoHoanHao/main.js
@@ -1,14 +1,14 @@
 let n = Number(prompt("Nhập số n vào: "));
-let array = [];
 for (let i = 1; i <= n; i++) {
   let s = 0;
+  // Tính tổng ước số = ?
   for (let j = 1; j < i; j++) {
     if (i % j === 0) {
       s += j;
     }
   }
+  // So sánh tổng ước số === s ?
   if (s === i) {
-    array.push(i);
+    console.log(i);
   }
 }
-console.log(array);
