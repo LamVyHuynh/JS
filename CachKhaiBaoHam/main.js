364eb98de5f8892bf8124ae444500c2ba51839c3
LamVyHuynh <huynhlamvy145@gmail.com>
2025-01-16 15:21:16 +0700

    cach khai bao ham ly thuyet

---
+++ CachKhaiBaoHam/main.js
@@ -0,0 +1,13 @@
+// Function declaration (khai báo tường minh)
+function tong(a, b) {
+  return a + b;
+}
+
+let cong = tong(2, 3);
+console.log(cong);
+
+// Function expression (khai báo tường biến)
+let tich = function (a, b) {
+  return a * b;
+};
+console.log(tich(2, 3));
